ff27166cc0ae94175e82e4102711fb91ad4fa552
G-Bursuc <c18399946@mytudublin.ie>
2020-12-02 19:11:45 +0000

    First push

--- src/screens/ChangeScreen.java
+++ src/screens/ChangeScreen.java
@@ -1,5 +1,6 @@
 package screens;
 
+import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 
@@ -10,6 +11,7 @@ public class ChangeScreen extends JFrame{
 	public ChangeScreen(Order shoppingBasket) {
 		// create elements
 		JPanel panel = new JPanel();
+		JButton calculateBttn = new JButton("Calculate");
 		
 		// add elements to the panel and add panel to the JFrame
 		add(panel);
